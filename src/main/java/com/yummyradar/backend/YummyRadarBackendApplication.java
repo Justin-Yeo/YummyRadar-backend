c3a0d00f6b4b5d4de05bb1a87405db4f6b7f2301
Justin-Yeo <156579337+Justin-Yeo@users.noreply.github.com>
2025-05-18 19:11:09 +0800

    Setup backend

---
+++ src/main/java/com/yummyradar/backend/YummyRadarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yummyradar.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummyRadarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YummyRadarBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yummyradar/backend/YummyRadarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yummyradar.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyRadarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
